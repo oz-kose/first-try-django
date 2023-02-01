c7e16d1316486d8f378004ca3659ed4f646b96b0
oz-kose <123957412+oz-kose@users.noreply.github.com>
2023-02-01 20:24:03 +0900

    part 15/16 update (stuck)
    don't have the damn references folder

--- first-try-django/trydjango/views.py
+++ first-try-django/trydjango/views.py
@@ -3,14 +3,28 @@ To render html web pages
 """
 from django.http import HttpResponse
 
-HTML_STRING = """
-<h1>EB showed me how to do it</h1>
-"""
+from articles import Article
 
 def home_view(request):
     """
     Take in a request (Django sends request)
     Return HTML as a response (We pick to return the response)
     """
+
+
+    #from database
+    article_obj = Article.objects.get(id=3)
+
+    H1_STRING = f"""
+    <h1>{article_obj.title} ({article_obj.id})</h1>
+    """
+    P1_STRING = f"""
+    <h1>{article_obj.content}</h1>
+    """
+    P2_STRING = """
+    <h1>EB showed me how to do it</h1>
+    """
+
+    HTML_STRING = H1_STRING + P1_STRING + P2_STRING
 
     return HttpResponse(HTML_STRING)
